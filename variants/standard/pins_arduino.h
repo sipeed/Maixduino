d00bce591458136bbf322669d9f17145c4dd2a36
Neucrack <czd666666@gmail.com>
2019-03-26 16:14:19 +0800

    add board Maix Go support

--- variants/standard/pins_arduino.h
+++ variants/standard/pins_arduino.h
@@ -1,6 +1,11 @@
 // API compatibility
-#ifdef SIPEED_M1_BOARD
+#ifdef BOARD_SIPEED_MAIX_ONE_DOCK
 
 #include "sipeedm1.h"
+#else
+
+#ifdef BOARD_SIPEED_MAIX_GO
+#include "sipeed_go.h"
+#endif //BOARD_SIPEED_MAIX_GO
 
 #endif
