ff02b8ea83daea0601d4a584c6bbe5f3a882a90a
Bigbits <bigbits@hackx.cc>
2019-05-04 17:43:19 +0800

    add serial.printf() function.

--- cores/arduino/Print.cpp
+++ cores/arduino/Print.cpp
@@ -28,6 +28,30 @@
 #include "Print.h"
 
 // Public Methods //////////////////////////////////////////////////////////////
+size_t Print::printf(const char *format, ...)
+{
+    char loc_buf[64];
+    char * temp = loc_buf;
+    va_list arg;
+    va_list copy;
+    va_start(arg, format);
+    va_copy(copy, arg);
+    size_t len = vsnprintf(NULL, 0, format, arg);
+    va_end(copy);
+    if(len >= sizeof(loc_buf)){
+        temp = new char[len+1];
+        if(temp == NULL) {
+            return 0;
+        }
+    }
+    len = vsnprintf(temp, len+1, format, arg);
+    write((uint8_t*)temp, len);
+    va_end(arg);
+    if(len >= sizeof(loc_buf)){
+        delete[] temp;
+    }
+    return len;
+}
 
 /* default implementation: may be overridden */
 size_t Print::write(const uint8_t *buffer, size_t size)
